1006ea1f498128966ce9a42c716fa48feba26f87
AriAndriana <andrianaari21@gmail.com>
2019-08-14 16:27:18 +0700

    looping

---
+++ XI RPL 2/loop/loop.php
@@ -0,0 +1,19 @@
+<?php
+$loop = 20;
+$num = 1;
+for ($i=0; $i <= $loop ; $i++) { 
+    if ($i == 1) {
+        echo "Aku Data ke - ". ${loop ." Dan mendapatkan mendali Emas<br>";        
+    }elseif ($loop == 2) {
+        echo "Aku Data ke - ". ${loop ." Dan mendapatkan mendali perak<br>";
+    }elseif ($loop == 3) {
+        echo "Aku Data ke - ". ${loop ." Dan mendapatkan mendali perunggu<br>";
+    }elseif ($loop >= 4) {
+        echo "Aku Data ke - ". ${loop ." Dan mendapatkan tas laptop<br>";
+    }elseif ($loop <= 9) {
+        echo "Aku Data ke - ". ${loop ." Dan mendapatkan tas karung<br>";
+    }elseif ($loop >= 10) {
+        echo "Aku Data ke - ". ${loop ." Dan tidak mendapatkan apapun<br>";
+    }
+}
+?>
